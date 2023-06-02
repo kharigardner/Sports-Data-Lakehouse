f365b88fa4b86ed25ec19154b50b1c18e80c67d0
Khari Gardner <kharigardner@protonmail.com>
2023-06-02 15:24:27 -0400

    fixed auth

--- orchestration/tasks/nba/functions.py
+++ orchestration/tasks/nba/functions.py
@@ -6,12 +6,13 @@ from nba_api.stats.endpoints._base import Endpoint
 
 import logging
 import typing
-
+import os
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+PROXY_INFO = os.environ.get('PROXY_INFO')
 
 def static_data_fetch() -> None:
     """
@@ -288,7 +289,6 @@ def all_time_leaders() -> None:
 def get_games() -> typing.List[typing.Dict[str, pd.DataFrame]]:
     TEAM_IDS = {1610612737,1610612738,1610612739,1610612740,1610612741,1610612742,1610612743,1610612744,1610612745,1610612746,1610612747,1610612748,1610612749,1610612750,1610612751,1610612752,1610612753,1610612754,1610612755,1610612756,1610612757,1610612758,1610612759,1610612760,1610612761,1610612762,1610612763
     ,1610612764,1610612765, 1610612766}
-    PROXY_INFO = 'http://groups-RESIDENTIAL:apify_proxy_X4cFIqR8cbXXz2hq2bTusiFAQctscK3bu1lI@proxy.apify.com:8000'
 
     teams_df = []
 
